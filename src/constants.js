5cce78d2d2d000fdd62d9531fcebd51df0ea5e22
Mark <marklin1106@gmail.com>
2021-08-08 21:08:49 +0800

    feat: Add some constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const DEFAULT_INPUT_INIT_VALUE = '';
+
+export const DEFAULT_SEPARATORS = [
+  ',',
+  ';',
+  '\n', // for copy past
+  '\r', // for copy past
+  '\r\n', // for copy past
+];
