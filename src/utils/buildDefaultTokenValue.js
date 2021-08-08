ddc1e0d9c4eda020e724f27d84b2d59a73c8b690
Mark <marklin1106@gmail.com>
2021-08-08 21:05:10 +0800

    feat: Add utils

---
+++ src/utils/buildDefaultTokenValue.js
@@ -0,0 +1,24 @@
+/**
+ * Default function for TokenInput to
+ * build default token value from user input value
+ *
+ * buildDefaultTokenValue(inputValue)
+ *
+ * @ inputValue
+ * Type: string
+ * Description: The user input value.
+ * Which is `one item` of the `user input string` split by the `separators`
+ * Example:
+ * - Input string "ABC, DEF" and separators is `,`
+ * - buildDefaultTokenValue will be called twice
+ * buildDefaultTokenValue('ABC') and buildDefaultTokenValue('DEF')
+ *
+ * @ return
+ * Type: string
+ * Description: The default token value
+ */
+const buildDefaultTokenValue = (inputValue) => {
+  return inputValue.trim();
+};
+
+export default buildDefaultTokenValue;
