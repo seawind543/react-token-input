d16dd0be6724fc72a536c37a7f57cc5b805945c5
Mark <marklin1106@gmail.com>
2021-08-14 14:18:36 +0800

    doc: Add and fix typo of commants

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,13 @@
-export const DEFAULT_INPUT_INIT_VALUE = '';
-
+/**
+ * The default `separators`
+ * in the `TokenLabel` (onGetTokenDisplayLabel)
+ */
 export const DEFAULT_SEPARATORS = [
   ',',
   ';',
-  '\n', // for copy past
-  '\r', // for copy past
-  '\r\n', // for copy past
+  '\n', // for copy and paste
+  '\r', // for copy and paste
+  '\r\n', // for copy and paste
 ];
 
 /**
@@ -13,3 +15,8 @@ export const DEFAULT_SEPARATORS = [
  * in the `TokenLabel` (onGetTokenDisplayLabel)
  */
 export const HARD_CODE_DELETE_BUTTON_CLASS_NAME = 'token-delete-button';
+
+/**
+ * The default init value of the `input`
+ */
+export const DEFAULT_INPUT_INIT_VALUE = '';
