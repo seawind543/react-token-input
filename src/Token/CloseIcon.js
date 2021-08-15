d66d7c5c13a23cce01a87f615c8a44a0cebb4ba7
Mark Lin <marklin1106@gmail.com>
2021-08-15 16:38:44 +0800

    feat: Enhance Token UI/UX
    feat: Enhance Token UI/UX
    (cherry picked from commit afb844c0a21f51967802847ef1f8181bc1e2cbe4)

---
+++ src/Token/CloseIcon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles.styl';
+
+const CloseIcon = () => {
+  return <div role="img" className={styles['close-icon']} aria-hidden="true" />;
+};
+
+export default CloseIcon;
--- src/Token/DeleteButton.jsx
+++ src/Token/DeleteButton.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import styles from '../styles.styl';
+import { HARD_CODE_DELETE_BUTTON_CLASS_NAME } from '../constants';
+import CloseIcon from './CloseIcon';
 
-const DeleteButton = () => {
+const DeleteButton = ({ onRenderContent }) => {
+  const isCustomizeContent = !!onRenderContent;
   return (
     <span
       role="button"
-      className={styles['delete-button']}
+      className={classNames(
+        HARD_CODE_DELETE_BUTTON_CLASS_NAME,
+        styles['delete-button']
+      )}
       aria-hidden="true"
-    />
+    >
+      {isCustomizeContent && onRenderContent()}
+      {!isCustomizeContent && <CloseIcon />}
+    </span>
   );
+};
+
+DeleteButton.propTypes = {
+  onRenderContent: PropTypes.fun,
 };
 
 export default DeleteButton;
