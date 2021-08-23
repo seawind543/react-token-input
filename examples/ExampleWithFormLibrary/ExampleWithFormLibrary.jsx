d6ab36795d47064889d24d2d9e1dcd23c462cda7
Mark Lin <marklin1106@gmail.com>
2021-08-23 13:29:59 +0800

    doc: Update example link for integrate with form library
    (cherry picked from commit 87ff6f9d5f80f86861ca61343ba2f7129e30cf11)

---
+++ examples/ExampleWithFormLibrary/ExampleWithFormLibrary.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import CopyAnchor from '../share/CopyAnchor';
+
+const ExampleWithFormLibrary = () => {
+  return (
+    <>
+      <h2>
+        Integrate with Form Library
+        <CopyAnchor hashTag="example-integrate-with-form-library" />
+      </h2>
+
+      <p>TokenInput could work within common form libraries.</p>
+
+      <p>
+        There are multiple ways we could apply to integrate TokenInput with a
+        form library.
+        <br />
+        Reference the link below for an live example about one of an integrate
+        ways.
+      </p>
+
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://codepen.io/seawind543/pen/VwbOwPa"
+      >
+        Example of integrated with a Form library
+      </a>
+    </>
+  );
+};
+
+export default ExampleWithFormLibrary;
