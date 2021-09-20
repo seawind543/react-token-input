41a8cb20574aa4cfcb21adb1cf324eb61a3c4cd1
Mark <marklin1106@gmail.com>
2021-09-20 11:30:31 +0800

    test: update getDefaultTokenEditableValue

--- src/utils/getDefaultTokenEditableValue.test.js
+++ src/utils/getDefaultTokenEditableValue.test.js
@@ -1,16 +1,12 @@
 import getDefaultTokenEditableValue from './getDefaultTokenEditableValue';
-import {
-  MOCK_TOKEN_VALUES,
-  MOCK_TOKEN_METAS,
-} from '../__fixtures__/defaultToken.fixtures';
+import { DEFAULT_VALUE_TYPE } from '../__fixtures__/tokens.fixtures';
 
 describe('getDefaultTokenEditableValue()', () => {
-  it('should return tokenValue`', () => {
-    MOCK_TOKEN_VALUES.forEach((tokenValue, idx) => {
-      const tokenMeta = MOCK_TOKEN_METAS[idx];
-      expect(getDefaultTokenEditableValue(tokenValue, tokenMeta)).toBe(
-        tokenValue
-      );
-    });
+  it('should return `tokenValue` directly', () => {
+    const { tokenValue, tokenMeta } = DEFAULT_VALUE_TYPE.BASED;
+
+    expect(getDefaultTokenEditableValue(tokenValue, tokenMeta)).toBe(
+      tokenValue
+    );
   });
 });
