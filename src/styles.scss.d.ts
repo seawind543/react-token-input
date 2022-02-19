dff5ac8bdd4963da4ce77a94e8cdc5a3418ff7ba
Mark <marklin1106@gmail.com>
2022-02-19 15:09:54 +0800

    chore: apply typed-scss-modules

---
+++ src/styles.scss.d.ts
@@ -0,0 +1,21 @@
+export type Styles = {
+  'autosized-wrapper': string;
+  container: string;
+  'container--errors': string;
+  'container--focused': string;
+  'delete-button__close-icon': string;
+  token: string;
+  'token__delete-button': string;
+  'token__label-wrapper': string;
+  'token--active': string;
+  'token--editable': string;
+  'token--error': string;
+  'token--read-only': string;
+  'token-list': string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
