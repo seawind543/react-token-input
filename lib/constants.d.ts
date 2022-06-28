d5a2572a3e13b2a1499a7e9510371447ded44a61
Mark <marklin1106@gmail.com>
2022-06-28 23:45:03 +0800

    chore: update yarn install change files

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -1,10 +1,10 @@
 import type { InputString, TokenSeparator } from './types/mix';
 import type { SpecialKeyDownConfig } from './types/specialKeyDown';
 export declare const DEFAULT_SEPARATORS: TokenSeparator[];
-export declare const KEY_DOWN_HANDLER_CONFIG_OPTION: {
+export declare const KEY_DOWN_HANDLER_CONFIG_OPTION: Readonly<{
     OFF: number;
     ON: number;
-};
+}>;
 export declare const DEFAULT_SPECIAL_KEY_DOWN_CONFIG: Required<SpecialKeyDownConfig>;
 export declare const JS__TOKEN__DELETE_BUTTON__CLASS_NAME = "js__token__delete-button";
 export declare const DEFAULT_INPUT_INIT_VALUE: InputString;
