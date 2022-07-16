6b7ddc74469236508ce017594fc850a45108d390
Mark <marklin1106@gmail.com>
2022-07-16 15:55:38 +0800

    build: upgrade devDependencies

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -2,8 +2,8 @@ import type { InputString, TokenSeparator } from './types/mix';
 import type { SpecialKeyDownConfig } from './types/specialKeyDown';
 export declare const DEFAULT_SEPARATORS: TokenSeparator[];
 export declare const KEY_DOWN_HANDLER_CONFIG_OPTION: Readonly<{
-    OFF: number;
-    ON: number;
+    OFF: 0;
+    ON: 1;
 }>;
 export declare const DEFAULT_SPECIAL_KEY_DOWN_CONFIG: Required<SpecialKeyDownConfig>;
 export declare const JS__TOKEN__DELETE_BUTTON__CLASS_NAME = "js__token__delete-button";
