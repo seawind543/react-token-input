8c7a2786d07e04d53112ebd3271e9bda6fa919b0
Mark <marklin1106@gmail.com>
2022-08-01 23:18:31 +0800

    feat: directly export TokenProps from index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 import TokenInput, { type TokenInputProps } from './TokenInput';
+import type { TokenProps } from './Token';
 import { JS__TOKEN__DELETE_BUTTON__CLASS_NAME, KEY_DOWN_HANDLER_CONFIG_OPTION } from './constants';
-export { TokenInputProps, JS__TOKEN__DELETE_BUTTON__CLASS_NAME, KEY_DOWN_HANDLER_CONFIG_OPTION, };
+export { TokenInputProps, TokenProps, JS__TOKEN__DELETE_BUTTON__CLASS_NAME, KEY_DOWN_HANDLER_CONFIG_OPTION, };
 export default TokenInput;
