f9fb155525b5ec2047e50f663fd8b66cb2404cfd
Mark <marklin1106@gmail.com>
2022-12-05 22:28:53 +0800

    feat: export default separators

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import type { TokenProps } from './Token';
 import {
   JS__TOKEN__DELETE_BUTTON__CLASS_NAME,
   KEY_DOWN_HANDLER_CONFIG_OPTION,
+  DEFAULT_SEPARATORS,
 } from './constants';
 
 export {
@@ -26,6 +27,13 @@ export {
   JS__TOKEN__DELETE_BUTTON__CLASS_NAME,
 
   /**
+   * @description
+   * The default value of the props.separators
+   */
+  DEFAULT_SEPARATORS,
+
+  /**
+   * @description
    * A constant object to help config the special keyDown event handler setting
    */
   KEY_DOWN_HANDLER_CONFIG_OPTION,
