40e0347b3eb4c92c4e7079b6cc0553c3e5fdbe73
Mark <marklin1106@gmail.com>
2024-07-24 14:39:53 +0800

    chore: upgrade typescript to v5.5.4

--- lib/hooks/useTokenInputFocusEffect.d.ts
+++ lib/hooks/useTokenInputFocusEffect.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 export interface TokenInputFocusHandler {
     (e?: React.FocusEvent<HTMLInputElement>): void;
 }
--- lib/utils/buildTokenMeta.d.ts
+++ lib/utils/buildTokenMeta.d.ts
@@ -1,3 +1,3 @@
 import type { Index, TokenMeta } from '../types/token';
-declare const buildTokenMeta: <VT, ET>(customizeError: import("../types/mix").Nullish | ET, value: VT, index: Index) => TokenMeta<ET>;
+declare const buildTokenMeta: <VT, ET>(customizeError: TokenMeta<ET>["error"], value: VT, index: Index) => TokenMeta<ET>;
 export default buildTokenMeta;
