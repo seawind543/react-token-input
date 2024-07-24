2cb8772a21366ebfdbb7d828f207b1febdfd5273
Mark <marklin1106@gmail.com>
2024-07-24 14:54:40 +0800

    build: upgrade css-loader, css-minimizer-webpack-plugin, mini-css-extract-plugin, postcss, prettier, sass-loader, style-loader, typed-scss-modules

--- src/styles.scss.d.ts
+++ src/styles.scss.d.ts
@@ -1,17 +1,17 @@
 export type Styles = {
-  "autosized-wrapper": string;
+  'autosized-wrapper': string;
   container: string;
-  "container--errors": string;
-  "container--focused": string;
-  "delete-button__close-icon": string;
+  'container--errors': string;
+  'container--focused': string;
+  'delete-button__close-icon': string;
   token: string;
-  "token__delete-button": string;
-  "token__label-wrapper": string;
-  "token--active": string;
-  "token--editable": string;
-  "token--error": string;
-  "token--read-only": string;
-  "token-list": string;
+  'token__delete-button': string;
+  'token__label-wrapper': string;
+  'token--active': string;
+  'token--editable': string;
+  'token--error': string;
+  'token--read-only': string;
+  'token-list': string;
 };
 
 export type ClassNames = keyof Styles;
