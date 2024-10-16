800b9be4a4ba46844da90d407a58dc6b515e88d3
Jatin Hariani <jtnh321@gmail.com>
2024-10-16 09:21:27 +0400

    Export Token component from the library

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 import TokenInput from './TokenInput';
 export type { TokenInputRef, TokenInputProps } from './TokenInput';
 export type { TokenProps } from './Token';
+export { default as Token } from './Token';
 export { JS__TOKEN__DELETE_BUTTON__CLASS_NAME, KEY_DOWN_HANDLER_CONFIG_OPTION, DEFAULT_SEPARATORS, } from './constants';
 export default TokenInput;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import TokenInput from './TokenInput';
 
 export type { TokenInputRef, TokenInputProps } from './TokenInput';
 export type { TokenProps } from './Token';
+export { default as Token } from './Token';
 export {
   JS__TOKEN__DELETE_BUTTON__CLASS_NAME,
   KEY_DOWN_HANDLER_CONFIG_OPTION,
